4b4cfed96c7cfb5cab57710e0ae6041a071ad81f
ANKUSH YADAV <97619583+ankushyadav1979@users.noreply.github.com>
2022-05-18 21:34:01 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,16 +1,63 @@
-var randomNumber1=Math.floor(Math.random()*6)+1;
-var randomDiceImage1="dice"+randomNumber1+".png";
-var randomImageSource1="images/"+randomDiceImage1;
-var image1=document.querySelectorAll("img")[0];
-image1.setAttribute("src",randomImageSource1);
-var randomNumber2=Math.floor(Math.random()*6)+1;
-var randomDiceImage2="dice"+randomNumber2+".png";
-var randomImageSource2="images/"+randomDiceImage2;
-var image2=document.querySelectorAll("img")[1];
-image2.setAttribute("src",randomImageSource2);
-if(randomNumber1>randomNumber2)
-document.querySelector("h1").innerHTML="Player 1 won";
-else if(randomNumber1===randomNumber2)
-document.querySelector("h1").innerHTML="Draw!!";
-else
-document.querySelector("h1").innerHTML="Player 2 won";
+var numberOfDrumButtons=document.querySelectorAll(".drum").length;
+
+for(i=0;i<numberOfDrumButtons;i++)
+{
+    document.querySelectorAll(".drum")[i].addEventListener("click",handleClick);
+
+}
+
+function handleClick() {
+var buttonInnerHTML=this.innerHTML;
+makeSound(buttonInnerHTML);
+buttonAnimation(buttonInnerHTML);
+
+}
+document.addEventListener("keydown",function(event) {
+  makeSound(event.key);
+  buttonAnimation(event.key);
+});
+
+function makeSound(key) {
+  switch (key) {
+    case "w":
+    var tom1=new Audio("sounds/tom-1.mp3");
+    tom1.play();
+    break;
+    case "a":
+    var tom2=new Audio("sounds/tom-2.mp3");
+    tom2.play();
+    break;
+    case "s":
+    var tom3=new Audio("sounds/tom-3.mp3");
+    tom3.play();
+    break;
+    case "d":
+    var tom4=new Audio("sounds/tom-4.mp3");
+    tom4.play();
+    break;
+    case "j":
+    var snare=new Audio("sounds/snare.mp3");
+    snare.play();
+    break;
+    case "k":
+    var crash=new Audio("sounds/crash.mp3");
+    crash.play();
+    break;
+    case "l":
+    var kick=new Audio("sounds/kick-bass.mp3");
+    kick.play();
+    break;
+
+
+    default:
+    console.log();
+
+}
+}
+function buttonAnimation(currentKey){
+  var activeButton=document.querySelector("."+currentKey);
+  activeButton.classList.add("pressed");
+  setTimeout(function(){
+    activeButton.classList.remove("pressed");
+  },100);
+}
